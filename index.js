97641e5e4fdb579d7d74f4334515abf9db7a18b7
Hitesh sharma <sharmahitesh9828@gmail.com>
2024-01-31 16:35:56 +0530

    create note-app

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const cors=require('cors')
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"http://127.0.0.1:5173/",
+    credentials:true
+}))
 
 app.use('/user',userRouter)
 app.use('/note' ,Noterouter)
